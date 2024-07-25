3df8cdfd0b672ba57f206faaad99671cc48ea1fa
yisus82 <djalma8@gmail.com>
2024-07-25 22:11:14 +0200

    Place colliders in room1

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -1,8 +1,15 @@
-import { setBackgroundColor } from './roomUtils.js';
+import { setBackgroundColor, setColliders } from './roomUtils.js';
 
 const room1 = (k, roomData) => {
   setBackgroundColor(k, '#A2AED5');
   const map = k.add([k.sprite('room1')]);
+  const layerObjects = roomData.layers.reduce((layerObjects, layer) => {
+    if (layer.type === 'objectgroup') {
+      layerObjects[layer.name] = layer.objects;
+    }
+    return layerObjects;
+  }, {});
+  setColliders(k, map, layerObjects.colliders);
 };
 
 export default room1;
--- src/scenes/roomUtils.js
+++ src/scenes/roomUtils.js
@@ -1,3 +1,29 @@
 export const setBackgroundColor = (k, color) => {
   k.add([k.rect(k.width(), k.height()), k.color(k.Color.fromHex(color)), k.fixed()]);
 };
+
+export const setColliders = (k, map, colliders) => {
+  for (const collider of colliders) {
+    if (collider.name === 'boss-barrier') {
+      continue;
+    }
+
+    let colliderShape;
+    if (collider.polygon) {
+      colliderShape = new k.Polygon(collider.polygon.map(point => k.vec2(point.x, point.y)));
+    } else {
+      colliderShape = new k.Rect(k.vec2(0), collider.width, collider.height);
+    }
+
+    map.add([
+      k.pos(collider.x, collider.y),
+      k.area({
+        shape: colliderShape,
+        collisionIgnore: ['collider'],
+      }),
+      k.body({ isStatic: true }),
+      'collider',
+      collider.type,
+    ]);
+  }
+};
