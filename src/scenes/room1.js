0a02b6a4d88faa976f4dc508a4fd96b2c2dfa144
yisus82 <djalma8@gmail.com>
2024-07-25 22:14:48 +0200

    Camera setup in room1

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -3,6 +3,7 @@ import { setBackgroundColor, setColliders } from './roomUtils.js';
 const room1 = (k, roomData) => {
   setBackgroundColor(k, '#A2AED5');
   const map = k.add([k.sprite('room1')]);
+
   const layerObjects = roomData.layers.reduce((layerObjects, layer) => {
     if (layer.type === 'objectgroup') {
       layerObjects[layer.name] = layer.objects;
@@ -10,6 +11,9 @@ const room1 = (k, roomData) => {
     return layerObjects;
   }, {});
   setColliders(k, map, layerObjects.colliders);
+
+  k.camScale(4);
+  k.camPos(170, 100);
 };
 
 export default room1;
